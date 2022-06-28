554e66c6d8a92d7f3b3b09c0b5cc453231a9eac2
Dylan Kendal <dylankendal@gmail.com>
2022-06-28 13:24:34 -0400

    fix: sign col overflow

--- plugin/coverage.lua
+++ plugin/coverage.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 local mark = vim.api.nvim_buf_set_extmark
-local group = vim.api.nvim_create_augroup("UserCoverage", { clear = true })
-local ns = vim.api.nvim_create_namespace("UserCoverage")
 
 local function find_coverage_file()
 	local path = vim.fn.findfile("coverage/coverage-final.json", ".;")
@@ -14,7 +12,7 @@ local function find_coverage_file()
 	return vim.fn.fnamemodify(path, ":p")
 end
 
-local function read_json(file)
+local function get_coverage(file)
 	local f = io.open(file, "r")
 	local buff = f:read("*a")
 	f:close()
@@ -22,7 +20,6 @@ local function read_json(file)
 	return vim.json.decode(buff)
 end
 
--- Replace vim.NIL with 0
 local function safe_num(v)
 	if v == vim.NIL then
 		return 0
@@ -35,115 +32,127 @@ local function mkloc(t)
 	return { safe_num(t.line) - 1, safe_num(t.column) }
 end
 
-local function apply_coverage()
-	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
-
-	local buf = vim.api.nvim_get_current_buf()
-
-	local current_file = vim.api.nvim_buf_get_name(buf)
-	current_file = vim.fn.fnamemodify(current_file, ":p")
-
-	local coverage_file = find_coverage_file()
-
-	if not coverage_file then
-		return
+local function to_sign_text(value)
+	if value > 99 then
+		return "+N"
 	end
 
-	local coverage = read_json(coverage_file)
-
-	local file_cov = coverage[current_file]
-
-	if not file_cov then
-		return
-	end
-
-	local statement_map = file_cov.statementMap
-	local branch_map = file_cov.branchMap
-	local fn_map = file_cov.fnMap
-
-	for key, value in pairs(file_cov.f) do
-		local fn = fn_map[key]
-		local s = mkloc(fn.decl.start)
-		local e = mkloc(fn.decl["end"])
-		if value == 0 then
-			mark(buf, ns, s[1], s[2], {
-				end_line = e[1],
-				end_col = e[2],
-				sign_text = " ",
-				sign_hl_group = "UserCoverageMiss",
-				hl_group = "UserCoverageMiss",
-			})
-		else
-			mark(buf, ns, s[1], s[2], {
-				end_line = e[1],
-				end_col = e[2],
-				sign_text = tostring(value),
-				sign_hl_group = "UserCoverageHit",
-				hl_group = "UserCoverageHit",
-			})
-		end
-	end
-
-	for key, value in pairs(file_cov.s) do
-		local statement = statement_map[key]
-		local s = mkloc(statement.start)
-		local e = mkloc(statement["end"])
-		if value == 0 then
-			mark(buf, ns, s[1], s[2], {
-				end_line = e[1],
-				end_col = e[2],
-				virt_text_pos = "right_align",
-				sign_text = " ",
-				sign_hl_group = "UserCoverageMiss",
-			})
-		else
-			mark(buf, ns, statement.start.line - 1, 0, {
-				virt_text_pos = "right_align",
-				sign_text = tostring(value),
-				sign_hl_group = "UserCoverageHit",
-			})
-		end
-	end
-
-	for key, values in pairs(file_cov.b) do
-		local item = branch_map[key]
-		local locations = item.locations
-
-		for idx, loc in ipairs(locations) do
-			if values[idx] == 0 then
-				local s = mkloc(loc.start)
-				local e = mkloc(loc["end"])
-				if item.type == "binary-expr" then
-					mark(buf, ns, s[1], s[2], {
-						end_line = e[1],
-						end_col = e[2],
-						hl_group = "UserCoveragerBranchNotTaken",
-					})
-				elseif item.type == "if" then
-					mark(buf, ns, s[1], s[2], {
-						virt_text_pos = "overlay",
-						conceal = "i",
-						virt_text = { { "if", "UserCoverageIfPathNotTaken" } },
-					})
-				end
-			end
-		end
-	end
+	return tostring(value)
 end
 
-local function init()
+local function main()
+	local group = vim.api.nvim_create_augroup("UserCoverage", { clear = true })
+	local ns = vim.api.nvim_create_namespace("UserCoverage")
+
 	vim.cmd([[hi! link UserCoverageMiss DiffDelete]])
 	vim.cmd([[hi! link UserCoverageHit DiffAdd]])
 	vim.cmd([[hi! UserCoveragerBranchNotTaken guibg=yellow]])
 	vim.cmd([[hi! UserCoverageIfPathNotTaken guibg=black guifg=yellow]])
 
-	vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 		group = group,
-		pattern = "*.ts,*.tsx,*.js,*.jsx",
-		callback = apply_coverage,
+		pattern = "*.ts",
+		callback = function()
+			vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+
+			local buf = vim.api.nvim_get_current_buf()
+			local current_file = vim.api.nvim_buf_get_name(buf)
+			current_file = vim.fn.fnamemodify(current_file, ":p")
+
+			local coverage_file = find_coverage_file()
+
+			if not coverage_file then
+				return
+			end
+
+			local coverage = get_coverage(coverage_file)
+
+			local file_cov = coverage[current_file]
+
+			if not file_cov then
+				return
+			end
+
+			local statement_map = file_cov.statementMap
+			local branch_map = file_cov.branchMap
+			local fn_map = file_cov.fnMap
+
+			for key, value in pairs(file_cov.f) do
+				local fn = fn_map[key]
+				local s = mkloc(fn.decl.start)
+				local e = mkloc(fn.decl["end"])
+				if value == 0 then
+					mark(buf, ns, s[1], s[2], {
+						end_line = e[1],
+						end_col = e[2],
+						sign_text = " ",
+						sign_hl_group = "UserCoverageMiss",
+						hl_group = "UserCoverageMiss",
+					})
+				else
+					mark(buf, ns, s[1], s[2], {
+						end_line = e[1],
+						end_col = e[2],
+						sign_text = to_sign_text(value),
+						sign_hl_group = "UserCoverageHit",
+						hl_group = "UserCoverageHit",
+					})
+				end
+			end
+
+			for key, value in pairs(file_cov.s) do
+				local statement = statement_map[key]
+				local s = mkloc(statement.start)
+				local e = mkloc(statement["end"])
+				if value == 0 then
+					mark(buf, ns, s[1], s[2], {
+						end_line = e[1],
+						end_col = e[2],
+						virt_text_pos = "right_align",
+						sign_text = " ",
+						sign_hl_group = "UserCoverageMiss",
+					})
+				else
+					mark(buf, ns, statement.start.line - 1, 0, {
+						sign_text = to_sign_text(value),
+						sign_hl_group = "UserCoverageHit",
+					})
+				end
+			end
+
+			for key, values in pairs(file_cov.b) do
+				local item = branch_map[key]
+				local locations = item.locations
+
+				for idx, loc in ipairs(locations) do
+					if values[idx] == 0 then
+						local s = mkloc(loc.start)
+						local e = mkloc(loc["end"])
+						if item.type == "binary-expr" then
+							mark(buf, ns, s[1], s[2], {
+								end_line = e[1],
+								end_col = e[2],
+								hl_group = "UserCoveragerBranchNotTaken",
+							})
+						elseif item.type == "if" then
+							mark(buf, ns, s[1], s[2], {
+								end_line = e[1],
+								end_col = e[2],
+								hl_group = "UserCoverageMiss",
+							})
+
+							mark(buf, ns, s[1], math.max(0, s[2] - 1), {
+								virt_text_pos = "overlay",
+								virt_text = { {"I", "UserCoverageIfPathNotTaken"} },
+							})
+						end
+					end
+				end
+			end
+		end,
 	})
 end
 
-init()
+main()
 
 return M
